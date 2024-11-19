f0a7c59ca8484a2b62bcaadb3ac2fc014e1474c2
Vigitoreanec <chirik39@mail.ru>
2024-11-19 20:27:12 +0200

    1. Создали консольное приложение для отображения запросов

---
+++ Beatify.Test.ConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Приложение начало работу");
+
+using (var client = new HttpClient())
+{
+    using var result = await client.GetAsync("https://localhost:7169/Song");
+    string content = await result.Content.ReadAsStringAsync();
+    Console.WriteLine(content);
+}
+
+Console.WriteLine("Приложение завершило работу");
