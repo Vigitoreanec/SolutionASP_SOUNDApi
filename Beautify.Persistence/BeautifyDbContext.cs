2dbdfaf84744d313a6b4dd91fc51526ff8d238ea
Vigitoreanec <chirik39@mail.ru>
2024-11-24 19:25:09 +0200

    1) Interfaces -> реализовали IBeautifyDbContext.cs который для определенной Базы данных, т.к. контекст Базы может быть разной 2) Установили в Beautify.Application ->EntityFrameworkCore

--- Beautify.Persistence/BeautifyDbContext.cs
+++ Beautify.Persistence/BeautifyDbContext.cs
@@ -1,10 +1,11 @@
-﻿using Beautify.PersistenceDb.EntityTypeConfiguration;
+﻿using Beautify.Application.Interfaces;
+using Beautify.PersistenceDb.EntityTypeConfiguration;
 using Microsoft.EntityFrameworkCore;
 using System.Text.RegularExpressions;
 
 namespace Beautify.PersistenceDb;
 
-public class BeautifyDbContext(DbContextOptions<BeautifyDbContext> options) : DbContext(options)
+public class BeautifyDbContext(DbContextOptions<BeautifyDbContext> options) : DbContext(options), IBeautifyDbContext
 {
     public DbSet<Group> Groups { get; set; }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
