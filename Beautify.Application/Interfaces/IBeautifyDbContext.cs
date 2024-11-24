9c0586ad4b2c0e8b16048c0087e74b33a0907236
Vigitoreanec <chirik39@mail.ru>
2024-11-24 19:25:09 +0200

    1) Interfaces -> реализовали IBeautifyDbContext.cs который для определенной Базы данных, т.к. контекст Базы может быть разной 2) Установили в Beautify.Application ->EntityFrameworkCore

---
+++ Beautify.Application/Interfaces/IBeautifyDbContext.cs
@@ -0,0 +1,11 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using System.Text.RegularExpressions;
+
+namespace Beautify.Application.Interfaces;
+
+public interface IBeautifyDbContext
+{
+    DbSet<Group> Groups { get; set; }
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+}
