3b6611b4c016db6ff18f2b52b69c87bbb92b3129
Vigitoreanec <chirik39@mail.ru>
2024-11-24 20:56:38 +0200

    1) Создали Интерфейс ОБОЩЕННЫЙ (дженерик <T>) interface IMapWidth<T> именно та сущность которая будет мапиться -> метод Mapping который будет возвращать объект от типа <T>  2) Реализация AssemblyMappingProfile от типа, проходя по всем типам будем проверяем содержание Интерфейса, который принимает дженерик тип и проверяем такой ли тип IMapWidth 3) Activator проходим по всем элементам, и проходя по всем элементам создаем тип (ТЕРМИНАТОР) рефлексия сопоставление от типа

---
+++ Beautify.Application/Common/MappingProfile/AssemblyMappingProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using System.Reflection;
+
+namespace Beautify.Application.Common.MappingProfile;
+
+public class AssemblyMappingProfile : Profile
+{
+    private void ApplyMappingsFromAssembly(Assembly assembly)
+    {
+        var types = assembly.GetExportedTypes()
+            .Where(type=>type.GetInterfaces()
+            .Any(i=>i.IsGenericType && 
+        i.GetGenericTypeDefinition() == typeof(IMapWith<>)))
+            .ToList();
+
+        foreach (var type in types)
+        {
+            var instance = Activator.CreateInstance(type);
+            var metodInfo = type.GetMethod("Mapping");
+            metodInfo?.Invoke(instance, [this]);
+        }
+    }
+}
---
+++ Beautify.Application/Common/MappingProfile/IMapWith.cs
@@ -0,0 +1,10 @@
+﻿
+using AutoMapper;
+
+namespace Beautify.Application.Common.MappingProfile;
+
+public interface IMapWith<T>
+{
+    void Mapping(Profile profile) =>
+        profile.CreateMap(typeof(T), GetType());
+}
